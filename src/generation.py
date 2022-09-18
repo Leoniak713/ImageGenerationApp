f45ef81ec63a6e42480db1a826136ceece7953f4
Maciej Leoniak <maciejleoniak@gmail.com>
2022-09-18 20:48:29 +0200

    Abstract ImageGenerator

--- src/generation.py
+++ src/generation.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Dict, Optional, Tuple
 
 import PIL
@@ -6,9 +7,9 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionImg2ImgPipeline, StableDiffusionPipeline
 
-class TextToImageGenerator:
+class ImageGenerator(ABC):
     """
-    Class for generating images from a text propmpt
+    Abstract class for image generators
     """
 
     MODEL_ID = "CompVis/stable-diffusion-v1-4"
@@ -19,6 +20,33 @@ class TextToImageGenerator:
     def __init__(self, device: str = 'cuda'):
         self.device = device
         self.model = self._get_model()
+    
+    @abstractmethod
+    def generate(
+        self, 
+        prompt: str, 
+        num_inference_steps: int, 
+        guidance_scale: float, 
+        generator_seed: Optional[int] = None,
+        *args,
+        **kwargs,
+    ) -> Optional[Dict]:
+        pass
+    
+    def _get_random_number_generator(self, seed: Optional[int]) -> Tuple[torch.Generator, int]:
+        random_number_generator = torch.Generator(self.device)
+        if seed is None:
+            generator_seed = random_number_generator.seed()
+        else:
+            random_number_generator.manual_seed(seed)
+            generator_seed = seed
+        return random_number_generator, generator_seed
+    
+
+class TextToImageGenerator(ImageGenerator):
+    """
+    Class for generating images from a text propmpt
+    """
     
     def generate(
         self, 
@@ -55,15 +83,6 @@ class TextToImageGenerator:
         model = StableDiffusionPipeline.from_pretrained(self.MODEL_ID, use_auth_token=True)
         return model.to(self.device)
     
-    def _get_random_number_generator(self, seed: Optional[int]) -> Tuple[torch.Generator, int]:
-        random_number_generator = torch.Generator(self.device)
-        if seed is None:
-            generator_seed = random_number_generator.seed()
-        else:
-            random_number_generator.manual_seed(seed)
-            generator_seed = seed
-        return random_number_generator, generator_seed
-    
     def _generate_image(
         self, 
         prompt: str, 
@@ -82,19 +101,10 @@ class TextToImageGenerator:
             )
         return model_output.images[0], model_output.nsfw_content_detected[0], random_number_generator_seed
 
-class ImageToImageGenerator:
+class ImageToImageGenerator(ImageGenerator):
     """
     Class for generating images from a text propmpt and a reference image
     """
-
-    MODEL_ID = "CompVis/stable-diffusion-v1-4"
-    HEIGHT = 512
-    WIDTH = 512
-    MAX_TRIES = 5
-    
-    def __init__(self, device='cuda'):
-        self.device = device
-        self.model = self._get_model()
     
     def generate(
         self, 
@@ -149,15 +159,6 @@ class ImageToImageGenerator:
         )
         return model.to(self.device)
     
-    def _get_random_number_generator(self, seed: Optional[int]) -> Tuple[torch.Generator, int]:
-        random_number_generator = torch.Generator(self.device)
-        if seed is None:
-            generator_seed = random_number_generator.seed()
-        else:
-            random_number_generator.manual_seed(seed)
-            generator_seed = seed
-        return random_number_generator, generator_seed
-    
     def _generate_image(
         self, 
         prompt: str, 
@@ -180,72 +181,3 @@ class ImageToImageGenerator:
                 return_dict=True,
             )
         return model_output.images[0], model_output.nsfw_content_detected[0], random_number_generator_seed
-
-class TextToImageGenerator:
-    """
-    Class for generating images from a text propmpt and a reference image
-    """
-    MODEL_ID = "CompVis/stable-diffusion-v1-4"
-    HEIGHT = 512
-    WIDTH = 512
-    MAX_TRIES = 5
-    
-    def __init__(self, device: str = 'cuda'):
-        self.device = device
-        self.model = self._get_model()
-    
-    def generate(
-        self, 
-        prompt: str, 
-        num_inference_steps: int, 
-        guidance_scale: float, 
-        latents: Optional[torch.Tensor] = None
-    ):
-        """
-        Args:
-            prompt (str)
-                Text to generate the image from.
-            num_inference_steps (int)
-                Number of GAN iterations, suggested default for preview - 10, suggested default for generation - 50.
-            guidance_scale (float)
-                How stronly related the image needs to be to the prompt, higher values cause drop in image quality, suggested default - 7.5.
-            generator_seed (torch.Tensor, *optional*)
-                The seed of the random number generator used for generating the input latent state. Provides deterministic output. If None, the seed will be randomly sampled.
-        """
-        if latents is None:
-            latents = self._get_latents()
-        max_tries = self.MAX_TRIES if latents is None else 1
-        for try_id in range(self.MAX_TRIES):
-            image, nsfw_detected = self._generate_image(prompt, num_inference_steps, guidance_scale, latents)
-            if nsfw_detected:
-                continue
-            return {
-                'image': image,
-                'latents': latents,
-            }
-        return None
-    
-    def _get_model(self):
-        model = StableDiffusionPipeline.from_pretrained(self.MODEL_ID, use_auth_token=True)
-        return model.to(self.device)
-    
-    def _get_latents(self):
-        latents_shape = (1, self.model.unet.in_channels, self.HEIGHT // 8, self.WIDTH // 8)
-        return torch.randn(latents_shape)
-    
-    def _generate_image(
-        self, 
-        prompt: str, 
-        num_inference_steps: int, 
-        guidance_scale: float, 
-        latents: torch.Tensor
-    ):
-        with autocast(self.device):
-            model_output = self.model(
-                prompt,
-                num_inference_steps=num_inference_steps,
-                guidance_scale=guidance_scale,
-                latents=latents,
-                return_dict=True,
-            )
-        return model_output.images[0], model_output.nsfw_content_detected[0]
